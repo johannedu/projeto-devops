8d0d713eb392fd2d019953f75b18ea65b6fb0fcd
Johann Eduardo <johanneduardo18@gmail.com>
2025-09-21 18:48:13 -0300

    Adiciona arquivos de teste unitário e configura workflow de ci

---
+++ script.test.js
@@ -0,0 +1,52 @@
+
+beforeEach(() => {
+    document.body.innerHTML = `
+        <input id="inputTarefa" />
+        <p id="mensagem"></p>
+        <ul id="listaTarefas"></ul>
+    `;
+
+    tarefas = [];
+});
+
+
+test('deve adicionar uma nova tarefa à lista', () => {
+    document.getElementById('inputTarefa').value = 'Comprar leite';
+    adicionarTarefa();
+    expect(tarefas).toContain('Comprar leite');
+});
+
+
+test('deve mostrar mensagem de erro se a tarefa for vazia', () => {
+    document.getElementById('inputTarefa').value = '';
+    adicionarTarefa();
+    const mensagem = document.getElementById('mensagem').textContent;
+    expect(mensagem).toBe('Valor vazio, tente novamente');
+});
+
+
+test('deve remover a tarefa correta da lista', () => {
+    tarefas.push('Tarefa A', 'Tarefa B');
+    removerTarefa(0); // Remove o primeiro item (Tarefa A)
+    expect(tarefas).not.toContain('Tarefa A');
+    expect(tarefas).toContain('Tarefa B');
+    expect(tarefas.length).toBe(1);
+});
+
+
+test('deve editar uma tarefa existente', () => {
+    tarefas.push('Tarefa Antiga');
+    
+
+    window.prompt = jest.fn(() => 'Tarefa Nova');
+    
+    editarTarefa(0);
+    expect(tarefas[0]).toBe('Tarefa Nova');
+});
+
+
+test('deve limpar o input após adicionar uma tarefa', () => {
+    document.getElementById('inputTarefa').value = 'Fazer exercício';
+    adicionarTarefa();
+    expect(document.getElementById('inputTarefa').value).toBe('');
+});
